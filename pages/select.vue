90b3951c10ee3e6f1e0ffe92ca39d98a2f8e3c0e
william <william810212@gmail.com>
2021-03-07 00:05:53 +0800

    add node-sass & sass-loader

---
+++ pages/select.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <h1 class="text-3xl">
+      text
+    </h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Select'
+}
+</script>
+
+<style lang="scss" scoped>
+@import url("~/assets/select.scss");
+</style>
