9bcbff87b31861f07cd3648e45759fb34f94af88
william <william810212@gmail.com>
2021-03-24 01:21:26 +0800

    add Roboto

--- nuxt.config.js
+++ nuxt.config.js
@@ -20,7 +20,8 @@ export default {
       { hid: 'description', name: 'description', content: '' }
     ],
     link: [
-      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
+      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
+      { rel: 'stylesheet', href: 'https://fonts.googleapis.com/css2?family=Roboto:wght@100;400;900&display=swap' }
     ]
   },
 
