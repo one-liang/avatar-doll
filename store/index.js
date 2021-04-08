a0a2c7120005a5e06a8a0ddeb97405becc6bff38
Liang <william810212@gmail.com>
2021-04-08 15:51:13 +0800

    add avatar parameter

---
+++ store/index.js
@@ -0,0 +1,89 @@
+export const state = () => ({
+  skin: 1,
+  body: 1,
+  eyes: 1,
+  glasses: 1,
+  nose: 1,
+  mouth: 1,
+  hair: 1,
+  book: 1,
+  animal: 1
+})
+
+export const mutations = {
+  changeSkin (state, payload) {
+    // eslint-disable-next-line no-console
+    console.log('payload', payload)
+    state.skin = payload
+  },
+  changeBody (state, payload) {
+    // eslint-disable-next-line no-console
+    console.log('payload', payload)
+    state.body = payload
+  },
+  changeEyes (state, payload) {
+    // eslint-disable-next-line no-console
+    console.log('payload', payload)
+    state.eyes = payload
+  },
+  changeGlasses (state, payload) {
+    // eslint-disable-next-line no-console
+    console.log('payload', payload)
+    state.glasses = payload
+  },
+  changeNose (state, payload) {
+    // eslint-disable-next-line no-console
+    console.log('payload', payload)
+    state.nose = payload
+  },
+  changeMouth (state, payload) {
+    // eslint-disable-next-line no-console
+    console.log('payload', payload)
+    state.mouth = payload
+  },
+  changeHair (state, payload) {
+    // eslint-disable-next-line no-console
+    console.log('payload', payload)
+    state.hair = payload
+  },
+  changeBook (state, payload) {
+    // eslint-disable-next-line no-console
+    console.log('payload', payload)
+    state.book = payload
+  },
+  changeAnimal (state, payload) {
+    // eslint-disable-next-line no-console
+    console.log('payload', payload)
+    state.animal = payload
+  }
+}
+
+export const getters = {
+  skinClass (state) {
+    return state.skin
+  },
+  bodyClass (state) {
+    return state.body
+  },
+  eyesClass (state) {
+    return state.eyes
+  },
+  glassesClass (state) {
+    return state.glasses
+  },
+  noseClass (state) {
+    return state.nose
+  },
+  mouthClass (state) {
+    return state.mouth
+  },
+  hairClass (state) {
+    return state.hair
+  },
+  bookClass (state) {
+    return state.book
+  },
+  animalClass (state) {
+    return state.animal
+  }
+}
