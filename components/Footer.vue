34a9545d64a908fee25c4333252a77ffe10876ff
william <william810212@gmail.com>
2021-04-12 16:04:58 +0800

    add footer

---
+++ components/Footer.vue
@@ -0,0 +1,14 @@
+<template>
+  <footer class="fixed bottom-0 left-0 w-full py-1 text-center bg-purple">
+    <p class="text-sm text-white">
+      Copyright © 2021 Liang
+      <span class="ml-2">Design by PinpinChen</span>
+    </p>
+  </footer>
+</template>
+
+<script>
+export default {
+  name: 'Footer'
+}
+</script>
