c6953488fcf2cef2057e26f2c4084a3b85b0de14
Liang <william810212@gmail.com>
2021-05-11 15:57:45 +0800

    chore: update layout

--- components/Starting.vue
+++ components/Starting.vue
@@ -1,8 +1,8 @@
 <template>
   <div :class="{'close': closeClass}" class="fixed inset-0 z-50 w-screen h-screen bg-gray-darker">
-    <button class="wrapper" type="button" @click="start">
+    <button class="wrap" type="button" @click="start">
       <svg height="60" width="320" xmlns="http://www.w3.org/2000/svg">
-        <rect class="shape" height="60" width="320" />
+        <rect class="box" height="60" width="320" />
       </svg>
       <div class="relative text-xl leading-8 tracking-widest text-center text-white -top-12">
         似顏繪開始
@@ -37,21 +37,21 @@ export default {
 </script>
 
 <style scoped>
-.wrapper {
+.wrap {
   @apply mx-auto w-80 block relative top-1/2 transform -translate-y-1/2 focus:outline-none;
 }
 
-.shape {
+.box {
   @apply stroke-current text-purple-lighter fill-current;
 
   stroke-dasharray: 140 540;
   stroke-dashoffset: -474;
-  stroke-width: 8px;
+  stroke-width: 10px;
   fill: transparent;
   transition: stroke-width 1s, stroke-dashoffset 1s, stroke-dasharray 1s;
 }
 
-.wrapper:hover .shape {
+.wrap:hover .box {
   stroke-width: 2px;
   stroke-dashoffset: 0;
   stroke-dasharray: 760;
